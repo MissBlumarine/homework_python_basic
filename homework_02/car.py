c5831d5411d4e6adade0f3247686cac8f6d946ad
MissBlumarine <miss_blumarine01@mail.ru>
2022-07-08 09:43:34 +0300

    hW2_3_6

--- homework_02/car.py
+++ homework_02/car.py
@@ -8,12 +8,10 @@ class Car(Vehicle):
 
     def __init__(self, weight, fuel, fuel_consumption, engine):
         super().__init__(weight, fuel, fuel_consumption)
-        self.engine = Engine
+        self.engine = engine
 
-    def set_engine(self, volume, pistons):
-        self.volume = volume
-        self.pistons = pistons
-        self.engine = Engine(volume=volume, pistons=pistons)
+    def set_engine(self):
+        self.engine = Engine(volume, pistons)
 
 
 
