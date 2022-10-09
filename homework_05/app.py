218a2350366b9fc678053edaa71d81f717711893
MissBlumarine <miss_blumarine01@mail.ru>
2022-10-09 17:09:41 +0300

    commit_1

---
+++ homework_05/app.py
@@ -0,0 +1,43 @@
+"""
+Домашнее задание №5
+Первое веб-приложение
+
+создайте базовое приложение на Flask
+создайте index view /
+добавьте страницу /about/, добавьте туда текст
+создайте базовый шаблон (используйте https://getbootstrap.com/docs/5.0/getting-started/introduction/#starter-template)
+в базовый шаблон подключите статику Bootstrap 5 и добавьте стили, примените их
+в базовый шаблон добавьте навигационную панель nav (https://getbootstrap.com/docs/5.0/components/navbar/)
+в навигационную панель добавьте ссылки на главную страницу / и на страницу /about/ при помощи url_for
+"""
+
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+app.config.update(
+    ENV="development",
+    SECRET_KEY="sdkjnslkdfgnajrsgoiqejrogj3qo4itgj",
+)
+
+
+@app.route("/", endpoint="index_page")
+def hello_world():
+    print_request()
+    return render_template("index.html")
+
+
+@app.route("/about/", endpoint="about_book")
+def hello_world():
+    print_request()
+    return render_template("book.html")
+
+
+def print_request():
+    print("request:", request)
+    print("request.method", request.method)
+    print("request.path", request.path)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
